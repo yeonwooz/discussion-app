e1b1f84f36a68cebf0f21cea3104b4347f96bdf3
suyeon woo <woosean999@gmail.com>
2022-09-15 10:29:12 +0900

    feature. 디스커션 나열기능 / agoraStatesDiscussions 배열의 데이터를 나열

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
+  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  const { author, avatarUrl, bodyHTML, createdAt, id, title, url, answer } =
+    obj;
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
@@ -13,11 +16,31 @@ const convertToDiscussion = (obj) => {
   const discussionAnswered = document.createElement("div");
   discussionAnswered.className = "discussion__answered";
 
-  // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  // custom
+  const discussionTitle = document.createElement("h3");
+  const discussionDate = document.createElement("time");
+  const userId = document.createElement("div");
 
+  // TODO: 레이아웃 지정
 
+  avatarWrapper.innerHTML = `<img class='discussion__avatar--img' src=${avatarUrl}  alt="user ${author}'s avatar">`;
+  userId.textContent = `${id}`;
 
-  li.append(avatarWrapper, discussionContent, discussionAnswered);
+  discussionTitle.innerHTML = `<a href=${url}>${title}</a>`;
+  discussionDate.textContent = `${createdAt}`;
+  discussionContent.innerHTML = `${bodyHTML}`;
+  if (answer) {
+    discussionAnswered.appendChild(convertToDiscussion(answer));
+  }
+
+  li.append(
+    avatarWrapper,
+    userId,
+    discussionTitle,
+    discussionDate,
+    discussionContent,
+    discussionAnswered
+  );
   return li;
 };
 
